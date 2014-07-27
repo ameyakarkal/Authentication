5839b268a5ced7de825a9c97ca8311c94b90d24a
akarkal@campuslabs.com <akarkal@campuslabs.com>
2014-07-27 08:58:38 -0400

    basic authentication using forms

---
+++ BasicFormsAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BasicFormsAuthentication.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username)
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                FormsAuthentication.RedirectFromLoginPage(username, true);    
+            }
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("index", "home");
+        }
+    }
+}
---
+++ BasicFormsAuthentication/Controllers/ResourceController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BasicFormsAuthentication.Controllers
+{
+    public class ResourceController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
